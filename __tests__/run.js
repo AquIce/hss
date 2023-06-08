c8d78753bdc10fca8eb60f4129f43b3e0f1e69b9
Timeo Perrinjaquet <Timeo.Perrinjaquet@gmail.com>
2023-06-08 12:09:39 +0000

    Added availability concept

--- __tests__/run.js
+++ __tests__/run.js
@@ -2,8 +2,10 @@ const { shell } = require('../src/hss.js');
 
 shell('vertex a 1')
 shell('vertex b')
+shell('vertex c')
 shell('link nand a a b')
 shell('update a')
 // shell('eval a b c')
 shell('assign not a, b')
 // shell('log')
+shell('vertices')
